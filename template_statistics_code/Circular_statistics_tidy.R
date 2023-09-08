9def9fe5b983e3877b5e6d1d6aa4817e250681b4
Juan Francisco Guarracino <jfguarracino@users.noreply.github.com>
2023-09-08 16:32:43 +0800

    Update Circular_statistics_tidy.R
    changed the plotting commands to get rid of the box outline and plot as a funtion of int_contrast instead of protocol

--- template_statistics_code/Circular_statistics_tidy.R
+++ template_statistics_code/Circular_statistics_tidy.R
@@ -16,7 +16,7 @@ rm(list = ls())
 graphics.off()
 
 # open data
-path <- 'C:\\data\\MEGAsync\\data\\behavioural_data\\selective_attention'
+path <- 'C:\\data\\juan\\MEGAsync\\data\\behavioural_data\\selective_attention'
 file <- 'res_sa.xlsx'
 
 data <- tibble()
@@ -43,16 +43,16 @@ data <- data %>%
 mdata <- data %>%
     group_by(protocol) %>%
     summarise(avg= mean.circular(run_fictrac_rot_rad), dev= sd.circular(run_fictrac_rot_rad),
-              mrl= rho.circular(run_fictrac_rot_rad)) %>%
+              mrl= rho.circular(run_fictrac_rot_rad), int_contrast= mean(int_contrast)) %>%
     mutate(dev= circular(dev, units= 'radians', modulo= '2pi'),
            mrl= circular(mrl, units= 'radians', modulo= '2pi')) %>%
-    mutate(lb= avg-dev, ub= avg+dev) #%>%
-  #  mutate(lb= ifelse(lb>0, lb, 0), ub= ifelse(ub<2*pi, ub, 2*pi)) # for plotting purposes only
+    mutate(lb= avg-dev, ub= avg+dev) %>%
+    mutate(lb= ifelse(lb>0, lb, 0), ub= ifelse(ub<2*pi, ub, 2*pi)) # for plotting purposes only
 # 
 # 
 # ##### plot data #####
 # # set color palette
-# palette <- c('#4527a0', '#283593', '#1565c0', '#0277bd', '#00838f', '#00695c', '#2e7d32','#558b2f')
+palette <- c('#4527a0', '#283593', '#1565c0', '#0277bd', '#00838f', '#00695c', '#2e7d32','#558b2f')
 # 
 # # The first step plots all the running directions along the outside of the circle
 # # (circle perimeter= 1) and colors the points based on treatment    
@@ -104,32 +104,41 @@ mdata <- data %>%
 
 ##### plot for single stimulus conditions #####
 sdata <- data %>%
-    filter(protocol== 1 | protocol== 8)
+    filter(protocol== 1 | protocol== 8) %>%
+    mutate(int_contrast= factor(int_contrast, levels= c(0, -0.95)))
 
-# smdata <- mdata %>%
-#     filter(protocol== 1 | protocol== 8)
+smdata <- mdata %>%
+    filter(protocol== 1 | protocol== 8) %>%
+    mutate(int_contrast= factor(int_contrast, levels= c(0, -0.95)))
 
 spalette <- c('#4527a0','#558b2f')
 
 # plot
-ggplot(data= smdata, aes(x= mrl, y= avg)) +
-    geom_point(data= sdata, aes(x= 1.15, y= run_fictrac_rot_rad, color= protocol), pch= 16,
-               alpha= 0.75, size= 3, position= position_jitter(width= 0.1)) +
-    geom_segment(aes(x= 0, y= avg, xend= mrl , yend= avg, color= protocol), linewidth= 1) +
-    geom_linerange(aes(x= mrl, ymin= lb, ymax= ub, color= protocol), linewidth= 1) +
-    geom_point(aes(fill = protocol),color = 'black',pch = 21,  stroke = 0.35, size = 3) +
-    theme_bw()+ theme(panel.grid.major= element_blank(), panel.grid.minor= element_blank(),
-                      legend.position= "none", text= element_text(size=14)) +
-    coord_polar(theta= 'y', start= rad(90), direction= 1, clip= 'off') +
-    geom_vline(xintercept = 1, color = "black", linewidth= 0.5) +
-    scale_y_continuous(limits=c(0, 2*pi), breaks= c(0, 90, 180, 270),
-                       labels= c(0,90,180,270)) +
-    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), 
-          axis.ticks.y=element_blank(), axis.title.x=element_blank(), 
-          axis.text.x=element_blank()) +
-    facet_wrap(~protocol, ncol= 4) +
-    theme(strip.text= element_blank()) +
-    scale_color_manual(values= spalette) + scale_fill_manual(values= spalette)
+fig2c <- ggplot(data= smdata, aes(x= mrl, y= avg)) +
+    geom_point(data= sdata, aes(x= 1.15, y= run_fictrac_rot_rad, color= int_contrast), pch= 16,
+              alpha= 0.75, size= 1, position= position_jitter(width= 0.1)) +
+    geom_segment(aes(x= 0, y= avg, xend= mrl , yend= avg, color= int_contrast), linewidth= 1) +
+    geom_linerange(aes(x= mrl, ymin= lb, ymax= ub, color= int_contrast), linewidth= 1) +
+    geom_point(aes(fill = int_contrast),color = 'black',pch = 21,  stroke = 0.35, size = 2) +
+    theme_bw()+
+    theme(panel.grid.major= element_blank(), panel.grid.minor= element_blank(),
+                       legend.position= "bottom", text= element_text(size= 8)) +
+     coord_polar(theta= 'y', start= rad(90), direction= 1, clip= 'off') +
+     geom_vline(xintercept = 1, color = "black", linewidth= 0.5) +
+     scale_y_continuous(limits=c(0, 2*pi), breaks= c(0, 90, 180, 270),
+                        labels= c(0,90,180,270)) +
+     theme(axis.title.y=element_blank(), axis.text.y=element_blank(), 
+           axis.ticks.y=element_blank(), axis.title.x=element_blank(), 
+           axis.text.x=element_blank()) +
+     facet_wrap(~int_contrast, ncol= 2) +
+     theme(strip.text= element_blank(), panel.border= element_blank()) +
+     scale_color_manual(values= spalette, name= 'luminance contrast\npolarisation contrast',
+                        labels= c('  0\n-0.95', '-0.95\n  0')) +
+     scale_fill_manual(values= spalette, name= 'luminance contrast\npolarisation contrast',
+                       labels= c('  0\n-0.95', '-0.95\n  0'))
+
+ggsave('C:\\Users\\22905562\\Desktop\\figure2c.pdf', plot= fig2c, width= 7.09, height= 2.36,
+       dpi= 300)
 
 # do the stats
 # keep only those columns of interest for bayesian analysis
@@ -148,11 +157,15 @@ run_single <- bsdata %>%
 
 ##### plot for dual loom only #####
 ddata <- data %>%
-    filter(protocol!= 1 & protocol!= 8)
+    filter(protocol!= 1 & protocol!= 8) %>%
+    mutate(int_contrast= factor(int_contrast, levels= c(-0.03125, -0.0625, -0.125, -0.25, -0.5,
+                                                        -0.95)))
 
 dmdata <- mdata %>%
     filter(protocol!= 1 & protocol!= 8) %>%
-  mutate(avg_deg= rad2deg(avg), dev_deg=rad2deg(dev))
+    mutate(avg_deg= rad2deg(avg), dev_deg=rad2deg(dev),
+           int_contrast= factor(int_contrast, levels= c(-0.03125, -0.0625, -0.125, -0.25, -0.5,
+                                                        -0.95)))
 
 # plot
 ggplot(data= dmdata, aes(x= mrl, y= avg)) +
@@ -174,6 +187,32 @@ ggplot(data= dmdata, aes(x= mrl, y= avg)) +
     theme(strip.text= element_blank()) +
     scale_color_manual(values= palette[2:7]) +
   scale_fill_manual(values= palette[2:7])
+
+fig3c <- ggplot(data= dmdata, aes(x= mrl, y= avg)) +
+    geom_point(data= ddata, aes(x= 1.15, y= run_fictrac_rot_rad, color= int_contrast), pch= 16,
+               alpha= 0.75, size= 1, position= position_jitter(width= 0.1)) +
+    geom_segment(aes(x= 0, y= avg, xend= mrl , yend= avg, color= int_contrast), linewidth= 1) +
+    geom_linerange(aes(x= mrl, ymin= lb, ymax= ub, color= int_contrast), linewidth= 1) +
+    geom_point(aes(fill = int_contrast),color = 'black',pch = 21,  stroke = 0.35, size = 2) +
+    theme_bw()+
+    theme(panel.grid.major= element_blank(), panel.grid.minor= element_blank(),
+          legend.position= "right", text= element_text(size= 8)) +
+    coord_polar(theta= 'y', start= rad(90), direction= 1, clip= 'off') +
+    geom_vline(xintercept = 1, color = "black", linewidth= 0.5) +
+    scale_y_continuous(limits=c(0, 2*pi), breaks= c(0, 90, 180, 270),
+                       labels= c(0,90,180,270)) +
+    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), 
+          axis.ticks.y=element_blank(), axis.title.x=element_blank(), 
+          axis.text.x=element_blank()) +
+    facet_wrap(~int_contrast, ncol= 3) +
+    theme(strip.text= element_blank(), panel.border= element_blank()) +
+    scale_color_manual(values= palette[2:7], name= 'luminance contrast',
+                       labels= c('-0.03', '-0.06', '-0.13', '-0.25', '-0.5', '-0.95')) +
+    scale_fill_manual(values= palette[2:7], name= 'luminance contrast',
+                      labels= c('-0.03', '-0.06', '-0.13', '-0.25', '-0.5', '-0.95'))
+
+ggsave('C:\\Users\\22905562\\Desktop\\figure3c.pdf', plot= fig3c, width= 7.09, height= 2.36,
+       dpi= 300)
 
 # do the stats
 bddata <- ddata %>%
